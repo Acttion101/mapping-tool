41f0d8df43a847a99f4de41b33a494813b0e3ac6
acttion101 <pongponpan102@gmail.com>
2021-07-30 21:51:21 +0700

    init Project

---
+++ data-mapping-tool/Server/Controllers/MappingController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JUST;
+using datamappingtool.Server.Models;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+
+namespace datamappingtool.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+  
+        public class MappingController : Controller
+        {
+            [HttpPost("tranform")]
+            public IActionResult Index(MappingModel mappingModel)
+            {
+
+         
+                string transformedString;
+                try
+                {
+                    if (string.IsNullOrEmpty(mappingModel.Input) || string.IsNullOrEmpty(mappingModel.Output))
+                    {
+                        var transform = JsonSerializer.Serialize(mappingModel.Output);
+                        var source = JsonSerializer.Serialize(mappingModel.Input);
+                        transformedString = JsonTransformer.Transform(transform, source);
+                    }
+                    else
+                    {
+                        var input = Regex.Replace(mappingModel.Input, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
+                        var output = Regex.Replace(mappingModel.Output, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
+                        transformedString = JsonTransformer.Transform(output, input);
+                    }
+                    return Ok(transformedString);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+        
+    }
+
+
---
+++ data-mapping-tool/Server/Models/MappingModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace datamappingtool.Server.Models
+{
+    public class MappingModel
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+}
